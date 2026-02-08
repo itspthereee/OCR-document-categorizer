e033254b973d3a987c19ca14ff724fd36439c037
Pisit Pongpan <2547pisit@gmail.com>
2026-02-08 16:36:24 +0700

    Fix CORS with wildcard

--- web_app.py
+++ web_app.py
@@ -23,8 +23,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 _reader: Optional[easyocr.Reader] = None
